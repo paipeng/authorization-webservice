23a3242c2ea006df0fb1f5ae4693049bda18cd04
Pai Peng <sipaipv6@gmail.com>
2021-12-01 14:43:23 +0800

    insert product to db

--- src/main/java/com/paipeng/authorization/service/ProductService.java
+++ src/main/java/com/paipeng/authorization/service/ProductService.java
@@ -2,6 +2,7 @@ package com.paipeng.authorization.service;
 
 import com.paipeng.authorization.config.ApplicationConfig;
 import com.paipeng.authorization.entity.Product;
+import com.paipeng.authorization.entity.User;
 import com.paipeng.authorization.repository.ProductRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -28,7 +29,19 @@ public class ProductService extends BaseService {
         return productRepository.findAll();
     }
 
-    public Product save(Product product) {
+    public Product save(Product product) throws Exception {
+        User currentUser = getUserFromSecurity();
+        if (currentUser == null) {
+            throw new Exception("403");
+        }
+
+        if (productRepository.findByBarcode(product.getBarcode()).orElse(null) != null) {
+            throw new Exception("409");
+        }
+
+        product.setUser(currentUser);
+
+        product = productRepository.saveAndFlush(product);
         return product;
     }
 
